06374a747cc3a27907f6dcc8f03c80d2b1a8902d
Gustav Kaleta <gustavkaleta@hotmail.com>
2018-10-31 21:58:03 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,9 +5,9 @@ variable "agent_count" {
     default = 3
 }
 
-variable "ssh_public_key" {
+variable "ssh_public_key" {}
    # //default = "~/.ssh/id_rsa.pub" #use this if running it locally
-}
+#}
 
 variable "dns_prefix" {
     default = "k8stest"
